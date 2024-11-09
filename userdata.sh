69b37e1677ce6ff050142695de7819debca95e3e
prabala <radhakrishnaprabala8@gmail.com>
2024-11-09 11:15:23 +0530

    userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -2,7 +2,7 @@
 # informing this as shell-script
 
 # directly connecting shell - rabbitmq and connect by above code
-cd /opt
+#cd /opt
 git clone https://github.com/prabalark/roboshop-72.git
 cd roboshop-72
 bash rabbitmq.sh ${rabbitm1_lg_password} &>>/opt/roboshop.log
